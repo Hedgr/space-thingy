42f54ebae9527c2e0a15e7ae1a8caca0ccfff39c
Hedgr <74436143+Hedgr@users.noreply.github.com>
2021-11-04 12:24:45 -0700

    Initial Commit

---
+++ game.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class hab_c {
+    public:
+    int max_air = 100;
+    int default_air = 75;
+    int max_integrity = 100;
+    int default_integrity = 100;
+    int food = 15;
+    int energy_per_turn = 5;
+    
+    int next_turn();
+    
+}
+
+int hab_c::next_turn() {
+    
+    return 0;
+}
+
+int main() {
+    
+    return 0;
+}
